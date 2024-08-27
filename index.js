41c9b750a654c1508d50cf0b5714b2e5c4d3c50b
Parshant2003 <105533918+Parshant2003@users.noreply.github.com>
2024-08-27 22:48:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:"http://localhost:3000",
+    origin:"https://xclone-i40mro08i-parshant-bishts-projects.vercel.app",
     credentials:true
 }
 app.use(cors(corsOptions));
